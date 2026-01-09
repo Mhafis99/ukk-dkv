5b4fbc60b91ab00f87dc87d7c1aefae7d2dc8a8c
Mhafis99 <mhafizhsetiawan82@gmail.com>
2026-01-09 15:11:22 +0700

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import { AuthProvider } from "@/components/auth-provider";
+
+import AuthProvider from "@/components/auth-provider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
